ef1e257be280a26f73a73d1c9782937cefddfc7f
migstt <113483557+migstt@users.noreply.github.com>
2024-05-08 18:17:16 +0800

    added social icons in navbar

---
+++ src/app/components/client/icons/LinkedInIcon.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const LinkedInIcon = (props: any) => (
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    height="1em"
+    role="img"
+    viewBox="0 0 24 24"
+    width="1em"
+    className="w-8 h-8 flex items-center justify-center rounded-lg"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M2.5 0a2.5 2.5 0 00-2.5 2.5v19a2.5 2.5 0 002.5 2.5h19a2.5 2.5 0 002.5-2.5V2.5A2.5 2.5 0 0021.5 0h-19zm4.34 19.718h-3.6v-11.44h3.6v11.44zm-1.8-12.678a1.8 1.8 0 111.8-1.8 1.8 1.8 0 01-1.8 1.8zm15.48 12.678h-3.6v-6.68c0-1.6-.64-2.52-2.12-2.52-1.12 0-1.76.76-2.06 1.52-.11.26-.14.66-.14 1v6.6h-3.6s.05-10.02 0-11.44h3.6v1.62c.49-.8 1.36-1.94 3.3-1.94 2.4 0 4.2 1.54 4.2 4.86v6.9z"
+    />
+  </svg>
+);
--- src/app/components/client/navbar.tsx
+++ src/app/components/client/navbar.tsx
@@ -3,7 +3,8 @@
 import React from "react";
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
 import ThemeSwitcher from "./switcher";
-
+import { GitHubIcon } from "./icons/GitHubIcon";
+import { LinkedInIcon } from "./icons/LinkedInIcon";
 
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
@@ -55,13 +56,23 @@ export default function NavBar() {
         <NavbarItem className="lg:flex">
           < ThemeSwitcher />
         </NavbarItem>
+        <NavbarItem className="lg:flex">
+          <Link color="foreground" href="https://github.com/migstt" target="_blank" rel="noopener noreferrer">
+            < GitHubIcon />
+          </Link>
+        </NavbarItem>
+        <NavbarItem className="lg:flex">
+          <Link color="foreground" href="https://www.linkedin.com/in/miguel-franco-trinidad-3a919b253/" target="_blank" rel="noopener noreferrer">
+            < LinkedInIcon />
+          </Link>
+        </NavbarItem>
       </NavbarContent>
       <NavbarMenu>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
               color={
-                index === 0 ? "primary" : "foreground"
+                index === 0 ? "secondary" : "foreground"
               }
               className="w-full"
               href="#"
