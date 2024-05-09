aac7708b8b6bdc36d8419ef9e4fbd01edd875298
migstt <113483557+migstt@users.noreply.github.com>
2024-05-09 17:48:10 +0800

    update large

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
-import NavBar from "./components/client/NavBar";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { cn } from "@/lib/utils"
+import { NavBar } from "@/components/general/NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "miguel.dev",
@@ -18,18 +22,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>
-        <Providers>
-          <header>
-            <NavBar />
-          </header>
-          <main>
-            {children}
-          </main>
-          <footer>
-            {/* To do */}
-          </footer>
-        </Providers>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NavBar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import MyImage from "./components/client/MyImage";
+import { Button } from "@/components/ui/button"
+import { ThemeSwitcher } from "@/components/general/ThemeSwitcher";
 
 
 export default function Home() {
   return (
-    <div className="sm:mx-[5%] md:mx-[10%] lg:mx-[15%] xl:mx-[20%] mt-10">
-      <MyImage />
-    </div>
+    <>
+      {/* NAVIGATION MENU */}
+
+
+
+      {/* <Button>samplers</Button>
+      <ModeToggle /> */}
+    </>
   );
 }
