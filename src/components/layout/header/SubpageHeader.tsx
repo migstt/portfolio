578b67b2ef8371c9425eb9e0f1be6f01423fec93
migstt <mft.trinidad@gmail.com>
2025-08-15 18:07:15 +0800

    sub page header logo and responsiveness

--- src/components/layout/header/SubpageHeader.tsx
+++ src/components/layout/header/SubpageHeader.tsx
@@ -1,15 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
 import { ThemeToggle } from "@/components/toggles/ThemeToggle";
-import { profile } from "@/data/portfolioData";
 import { HeaderActions } from "@/components/layout/header/HeaderActions";
+import { profile } from "@/data/portfolioData";
 
 export function SubpageHeader() {
   const { name } = profile;
+
   return (
     <header className="w-full py-4 px-4">
-      <div className="flex flex-col sm:flex-row items-center justify-between gap-3">
-        <div className="flex items-center gap-2">
-          <h1 className="text-xl font-bold truncate md:text-2xl">{name}</h1>
-        </div>
+      <div className="flex items-center justify-between gap-3">
+        <Link href="/" className="flex items-center gap-2">
+          <Image
+            src="/images/m.png"
+            alt="Logo"
+            width={30}
+            height={30}
+            priority
+            className="rounded-md"
+          />
+          <h1 className="hidden sm:block text-sm md:text-xl font-bold truncate">
+            {name}
+          </h1>
+        </Link>
+
+        {/* Actions and theme toggle */}
         <div className="flex items-center gap-3">
           <HeaderActions />
           <ThemeToggle />
