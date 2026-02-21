f473328c55267f518928e6b677807605436f0b74
migstt <113483557+migstt@users.noreply.github.com>
2026-02-21 20:49:46 +0800

    remove static export config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
   trailingSlash: true,
   reactStrictMode: false,
-  images: {
-    unoptimized: true,
-  },
   allowedDevOrigins: [
     "127.0.0.1",
     "localhost"
