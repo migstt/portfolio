a1fa1d18f090753bf0b29d4665ccdec9c26d5526
migstt <113483557+migstt@users.noreply.github.com>
2026-02-21 20:55:29 +0800

    wrap async components in suspense to fix animation replay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { Metadata } from "next";
 import { Layout } from "@/components/layout/MainLayout";
 import { About } from "@/components/cards/About";
@@ -46,12 +47,16 @@ export default async function Home() {
               <TechStack />
             </div>
             <div className="lg:col-span-3 md:col-span-1 h-full min-h-0 animate-slide-up-4">
-              <Projects />
+              <Suspense>
+                <Projects />
+              </Suspense>
             </div>
           </div>
           <div className="grid grid-cols-1 lg:grid-cols-7 md:grid-cols-2 gap-2">
             <div className="lg:col-span-3 md:col-span-1 flex flex-col gap-4 animate-slide-up-5">
-              <OffKeyboard />
+              <Suspense>
+                <OffKeyboard />
+              </Suspense>
             </div>
             <div className="lg:col-span-4 md:col-span-1 flex flex-col animate-slide-up-6">
               <TechBlog />
